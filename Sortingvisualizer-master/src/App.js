0bcfe45e4dafa5c1131008b28185b210d60d4884
Arun Sharma <62569196+Arun-web@users.noreply.github.com>
2022-10-31 01:19:09 +0530

    Add files via upload

---
+++ Sortingvisualizer-master/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SortingVisualizer from './Sortingvisualizer/Sortingvisualizer';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
